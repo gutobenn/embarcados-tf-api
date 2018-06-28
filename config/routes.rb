218f6ad4f519b4606465adf7e337085f5a08479a
Augusto Bennemann <gutobenn@gmail.com>
2018-06-27 20:55:25 -0400

    oauth with doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
+require 'api_constraints'
 Rails.application.routes.draw do
-  devise_for :users
-  namespace :api do
-    namespace :v1 do
+  use_doorkeeper do
+    # No need to register client application
+    skip_controllers :applications, :authorized_applications
+  end
+  scope module: :api, defaults: { format: :json }, path: 'api' do
+    scope module: :v1, constraints: ApiConstraints.new(version:1, default: true) do
+      devise_for :users, controllers: {
+        registrations: 'api/v1/users/registrations',
+      }, skip: [:sessions, :password]
       resources :compras
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
